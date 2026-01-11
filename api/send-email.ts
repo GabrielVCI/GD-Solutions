f2f341717eb23b8c99be50ed00eee198c338ec34
GabrielVCI <gabrielgamercavalier@gmail.com>
2026-01-11 19:47:12 -0400

    cambios 3

--- api/send-email.ts
+++ api/send-email.ts
@@ -143,8 +143,8 @@ import React from "react";
 import { Resend } from "resend";
 
 // OJO: tus templates pueden quedarse en .jsx/.tsx
-import ContactAdminEmail from "./_emails/ContactAdminEmail.ts"//"../react-email-starter/emails/ContactAdminEmail.js";
-import ContactClientEmail from "./_emails/ContactClientEmail.ts";
+import ContactAdminEmail from "./_emails/ContactAdminEmail"//"../react-email-starter/emails/ContactAdminEmail.js";
+import ContactClientEmail from "./_emails/ContactClientEmail";
 
 type ApiResponse =
   | { ok: true }
